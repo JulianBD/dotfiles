4aae4fd0576f23e8509372f597538606edeba9f9
Julian Dorsey <j.dorsey@elsevier.com>
2023-01-23 20:49:07 -0500

    Update 01/23/2023

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,7 +8,7 @@
 
 (menu-bar-mode -1)
 
-(set-face-attribute 'default nil :font "Hack" :height 160)
+(set-face-attribute 'default nil :font "Iosevka Comfy" :height 180)
 
 
 ;; Make ESC quit prompts
@@ -37,7 +37,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("1fab98300b100a19010734a14c4bf9b6712ffc8b9e1d7eca35f837adeeabf740" "53585ce64a33d02c31284cd7c2a624f379d232b27c4c56c6d822eff5d3ba7625" "986cdc701d133f7c6b596f06ab5c847bebdd53eb6bc5992e90446d2ddff2ad9e" default))
+   '("97820f47e5b99efd4d69430d29109405c970ca55415cf8ff6a1b844de9e01d86" "1dbbf33c64f2fa781b0f68494c0edee535d3a9f0c7e106b047d21c719c779a30" "1fab98300b100a19010734a14c4bf9b6712ffc8b9e1d7eca35f837adeeabf740" "53585ce64a33d02c31284cd7c2a624f379d232b27c4c56c6d822eff5d3ba7625" "986cdc701d133f7c6b596f06ab5c847bebdd53eb6bc5992e90446d2ddff2ad9e" default))
  '(package-selected-packages '(command-log-mode use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -70,11 +70,16 @@
 (use-package org-modern
   :config (global-org-modern-mode))
 
+;; magit
+(use-package magit
+  :config (magit-mode))
+
 ;; which-key
 (use-package which-key
+  :init
+    (setq which-key-popup-type 'minibuffer)
   :config
-  (setq which-key-popup-type 'minibuffer)
-  (which-key-mode))
+    (which-key-mode))
 
 ;; smex
 (use-package smex)
@@ -94,22 +99,136 @@
   :config
   (evil-collection-init))
 (use-package evil-org
-  :init (evil-org-mode))
+  :after org
+  :hook (org-mode . (lambda () evil-org-mode))
+  :config
+  (require 'evil-org-agenda)
+  (evil-org-agenda-set-keys))
 
 ;; ivy & counsel
-(use-package counsel)
+(use-package counsel
+  :config (counsel-mode))
 (use-package ivy
-  :init (ivy-mode))
+  :config (ivy-mode))
 (use-package all-the-icons-ivy-rich
   :config (setq all-the-icons-ivy-rich-icon t))
 (use-package ivy-rich
-  :init (ivy-rich-mode))
+  :init (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
+  :config (ivy-rich-mode 1))
 
 ;; projectile
-(use-package projectile)
+(use-package projectile
+  :bind (:map projectile-mode-map
+         ("C-c p" . projectile-command-map))
+  :config (projectile-mode +1))
+(use-package counsel-projectile
+  :config (counsel-projectile-mode))
 
-;; neotree
-(use-package neotree)
+;; treemacs
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+          treemacs-deferred-git-apply-delay        0.5
+          treemacs-directory-name-transformer      #'identity
+          treemacs-display-in-side-window          t
+          treemacs-eldoc-display                   'simple
+          treemacs-file-event-delay                2000
+          treemacs-file-extension-regex            treemacs-last-period-regex-value
+          treemacs-file-follow-delay               0.2
+          treemacs-file-name-transformer           #'identity
+          treemacs-follow-after-init               t
+          treemacs-expand-after-init               t
+          treemacs-find-workspace-method           'find-for-file-or-pick-first
+          treemacs-git-command-pipe                ""
+          treemacs-goto-tag-strategy               'refetch-index
+          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
+          treemacs-hide-dot-git-directory          t
+          treemacs-indentation                     2
+          treemacs-indentation-string              " "
+          treemacs-is-never-other-window           nil
+          treemacs-max-git-entries                 5000
+          treemacs-missing-project-action          'ask
+          treemacs-move-forward-on-expand          nil
+          treemacs-no-png-images                   nil
+          treemacs-no-delete-other-windows         t
+          treemacs-project-follow-cleanup          nil
+          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+          treemacs-position                        'left
+          treemacs-read-string-input               'from-child-frame
+          treemacs-recenter-distance               0.1
+          treemacs-recenter-after-file-follow      nil
+          treemacs-recenter-after-tag-follow       nil
+          treemacs-recenter-after-project-jump     'always
+          treemacs-recenter-after-project-expand   'on-distance
+          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+          treemacs-project-follow-into-home        nil
+          treemacs-show-cursor                     nil
+          treemacs-show-hidden-files               t
+          treemacs-silent-filewatch                nil
+          treemacs-silent-refresh                  nil
+          treemacs-sorting                         'alphabetic-asc
+          treemacs-select-when-already-in-treemacs 'move-back
+          treemacs-space-between-root-nodes        t
+          treemacs-tag-follow-cleanup              t
+          treemacs-tag-follow-delay                1.5
+          treemacs-text-scale                      nil
+          treemacs-user-mode-line-format           nil
+          treemacs-user-header-line-format         nil
+          treemacs-wide-toggle-width               70
+          treemacs-width                           35
+          treemacs-width-increment                 1
+          treemacs-width-is-initially-locked       t
+          treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+    (when treemacs-python-executable
+      (treemacs-git-commit-diff-mode t))
+
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple)))
+
+    (treemacs-hide-gitignored-files-mode nil))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t d"   . treemacs-select-directory)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(use-package treemacs-evil
+  :after (treemacs evil)
+  :ensure t)
+
+(use-package treemacs-projectile
+  :after (treemacs projectile)
+  :ensure t)
+
+(use-package treemacs-icons-dired
+  :hook (dired-mode . treemacs-icons-dired-enable-once)
+  :ensure t)
+
+(use-package treemacs-magit
+  :after (treemacs magit)
+  :ensure t)
 
 ;; mood-line
 (use-package mood-line
@@ -117,12 +236,35 @@
   (mood-line-mode))
 
 ;; modus-themes
-(use-package modus-themes
-  :init (load-theme 'modus-operandi-tinted))
+(use-package ef-themes
+  :init (load-theme 'ef-cherie))
+(use-package modus-themes)
 
 ;;; languages
 ;; shell
 ;; active Babel languages
 (org-babel-do-load-languages 'org-babel-load-languages '((shell . t)))
+
 ;; go-mode
 (use-package go-mode)
+
+;; groovy-mode
+(use-package groovy-mode)
+
+;; javascript-mode
+(use-package js2-mode)
+
+;; json-mode
+(use-package json-mode)
+
+;; jsonnet-mode
+(use-package jsonnet-mode)
+
+;; markdown-mode
+(use-package markdown-mode)
+
+;; terraform-mode
+(use-package terraform-mode)
+
+;; yaml-mode
+(use-package yaml-mode)
